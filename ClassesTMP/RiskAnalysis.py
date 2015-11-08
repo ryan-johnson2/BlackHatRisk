10d1e1ad699a08deb1067b2f7b92f8eb7228d5e1
John Humelsine <john.humelsine@usma.edu>
2015-11-08 18:24:26 -0500

    classes added
    controller, main, model, riskAnalysis.

---
+++ ClassesTMP/RiskAnalysis.py
@@ -0,0 +1,18 @@
+class RiskAnalysis:
+	def __init__ (self):
+		pass
+
+	def evalNetwork:
+		pass
+
+	def evalLink:
+		pass
+
+	def evalNode:
+		pass
+
+	def findLeastRisk:
+		pass
+
+	def findRandomRoute:
+		pass
---
+++ ClassesTMP/controller.py
@@ -0,0 +1,8 @@
+import model.py
+import view.py
+
+class Controller:
+
+	def __init__(self, model, gui):
+		self.model = Model()
+		self.gui = View()
