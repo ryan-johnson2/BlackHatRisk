9036f6b77dfe54b4a2fb8fc9337a3ecac8c7f943
unknown <x61366@USMANBUX61366.usma.ds.army.edu>
2016-01-12 10:35:57 -0500

    added link and node class
    Signed-off-by: unknown <x61366@USMANBUX61366.usma.ds.army.edu>

---
+++ src/link.py
@@ -0,0 +1,8 @@
+class Link:
+
+    def __init__(self, node1, node2, name, protocol, risk):
+        self.n1 = node1
+        self.n2 = node2
+        self.name = name
+        self.proto = protocol
+        self.risk = risk
---
+++ src/node.py
@@ -0,0 +1,7 @@
+class Node:
+
+    def __init__(self, name, storage):
+        self.name = name
+        self.storage = storage
+
+
